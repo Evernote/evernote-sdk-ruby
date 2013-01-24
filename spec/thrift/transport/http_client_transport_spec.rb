ebbe47de84a7505a3ba45ee420a21fea35f84aa4
Paul Salaets <psalaets@gmail.com>
2013-01-24 12:04:48 -0500

    tests for how http transport uses Net::HTTP

---
+++ spec/thrift/transport/http_client_transport_spec.rb
@@ -0,0 +1,91 @@
+require 'evernote-thrift'
+require 'fakeweb'
+require 'net/http'
+
+describe Thrift::HTTPClientTransport do
+  before :each do
+    FakeWeb.allow_net_connect = false
+    FakeWeb.register_uri(:any, 'http://www.evernote.com', :body => '')
+    FakeWeb.register_uri(:any, 'https://www.evernote.com', :body => '')
+  end
+
+  after :each do
+    FakeWeb.clean_registry
+    FakeWeb.allow_net_connect = true
+  end
+
+  it "should send content-type header by default" do
+    trans = Thrift::HTTPClientTransport.new('http://www.evernote.com')
+    trans.flush
+
+    req = FakeWeb.last_request
+
+    req.key?('content-type').should be_true
+  end
+
+  it "should send user-agent header by default" do
+    trans = Thrift::HTTPClientTransport.new('http://www.evernote.com')
+    trans.flush
+
+    req = FakeWeb.last_request
+
+    req.key?('user-agent').should be_true
+  end
+
+  it "should send user-supplied headers" do
+    trans = Thrift::HTTPClientTransport.new('http://www.evernote.com')
+    trans.add_headers('animal' => 'dog', 'age' => '10')
+    trans.flush
+
+    req = FakeWeb.last_request
+
+    req['animal'].should eq('dog')
+    req['age'].should eq('10')
+  end
+
+  it "should accumulate user-supplied headers" do
+    trans = Thrift::HTTPClientTransport.new('http://www.evernote.com')
+    trans.add_headers('animal' => 'dog')
+    trans.add_headers('age' => '10')
+    trans.flush
+
+    req = FakeWeb.last_request
+
+    req['animal'].should eq('dog')
+    req['age'].should eq('10')
+  end
+
+  it "should use most recent value when user-supplied header is repeated" do
+    trans = Thrift::HTTPClientTransport.new('http://www.evernote.com')
+    trans.add_headers('name' => 'first')
+    trans.add_headers('name' => 'last')
+    trans.flush
+
+    req = FakeWeb.last_request
+
+    req['name'].should eq('last')
+  end
+
+  it "should use ssl when url is https" do
+    # Lifted from HTTParty
+    http = Net::HTTP.new('www.evernote.com', 443, nil, nil)
+    response = stub(Net::HTTPResponse, :[] => '', :body => '', :to_hash => {})
+    http.stub(:post).and_return(response)
+    Net::HTTP.should_receive(:new).with('www.evernote.com', 443, nil, nil).and_return(http)
+    http.should_receive(:use_ssl=).with(true)
+
+    trans = Thrift::HTTPClientTransport.new('https://www.evernote.com')
+    trans.flush
+  end
+
+  it "should not use ssl when url is http" do
+    http = Net::HTTP.new('www.evernote.com', 80, nil, nil)
+    response = stub(Net::HTTPResponse, :[] => '', :body => '', :to_hash => {})
+    http.stub(:post).and_return(response)
+    Net::HTTP.should_receive(:new).with('www.evernote.com', 80, nil, nil).and_return(http)
+    http.should_receive(:use_ssl=).with(false).at_most(:once)
+
+    trans = Thrift::HTTPClientTransport.new('http://www.evernote.com')
+    trans.flush
+  end
+end
